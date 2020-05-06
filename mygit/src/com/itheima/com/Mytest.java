72ac3d51fef92061ede59ca14da4762a3f8c90a7
gp <13556677913@163.com>
2020-05-06 22:09:53 +0800

    测试远端更新

--- mygit/src/com/itheima/com/Mytest.java
+++ mygit/src/com/itheima/com/Mytest.java
@@ -4,5 +4,6 @@ public class Mytest {
     public static void main(String[] args) {
         System.out.println("本地提交到远端");
         System.out.println("重新提交");
+        System.out.println("测试远端更新");
     }
 }
